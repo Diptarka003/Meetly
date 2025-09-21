0bf8b6cc316a9567a6fa3c30ff74b9567fc28402
Diptarka <diptarkamukherjee.work@gmail.com>
2025-09-21 19:38:32 +0530

    added the upcoming and previous page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,10 +1,11 @@
+import CallList from '@/components/CallList'
 import React from 'react'
 
 const Previous = () => {
     return (
     <section className='flex size-full flex-col gap-10 text-white'>
        <div className='text-3xl font-bold'>
-         Previous
+         <CallList type='ended'/>
        </div>
     </section>
   )
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -50,7 +50,7 @@ const MeetingTypeList = () => {
 
 
  const router=useRouter()
- const meetingLink=${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}
+ const meetingLink=`${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`
  return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
        <HomeCard
@@ -117,13 +117,12 @@ const MeetingTypeList = () => {
               OnClose={()=>setMeetingState(undefined)}
               title="Meeting Created"
               className="text-center"
-              buttonText="Start Meeting"
               handleClick={()=>{
                 navigator.clipboard.writeText(meetingLink)
               }}
               image='/icons/checked.svg'
+              buttonText="Copy Meeting Link"
               buttonIcon='/icons/copy.svg'
-              buttonText='Copy Meeting Link'
             />
        }
 
